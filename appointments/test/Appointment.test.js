2dab412db99aa4f5a64f3026ead19e9677de8012
Dan Harwood <dannyharwood@hotmail.co.uk>
2022-06-29 19:41:05 +0100

    Passed the first test case.

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,1 +1,19 @@
-describe('Appointment', () => {});
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Appointment } from '../src/Appointment';
+
+describe('Appointment', () => {
+  it('renders the customers first name', () => {
+    // Arrange
+    const customer = { firstName: 'Ashley' };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+
+    // Act
+    ReactDOM.render(component, container);
+
+    // Assert
+    expect(document.body.textContent).toMatch('Ashley');
+  });
+});
